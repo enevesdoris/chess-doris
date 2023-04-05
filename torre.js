898bf11a461f80bae85f876dbb22458fab8f60c5
Eduardo Neves <eneves@doris.mobi>
2023-04-05 10:46:47 -0300

    atualizado os códigos em JS para adicionar opções de movimento para as torres

--- torre.js
+++ torre.js
@@ -1,31 +1,44 @@
-const torre = document.querySelector('.torre');
-const tabuleiro = document.querySelector('.coner');
+const torres = document.querySelectorAll('.torre'); // sao 4 torres (all)
+const tabuleiro = document.querySelector('.coner'); // tabuleiro só tem um
 
-tabuleiro.addEventListener('mouseover', mostrarOpcoesDeMovimento);
-tabuleiro.addEventListener('mouseleave', esconderOpcoesDeMovimento);
+Array.from(torres).forEach(torre => {  //usei o forEach como Loop para acessar as torres. 'foreach' = condiçaõ que se repete percorrendo uma Lista até que receba uma informação 'False'. 
+  torre.addEventListener('mouseover', mostrarOpcoesDeMovimento); //chama minha função
+  torre.addEventListener('mouseleave', esconderOpcoesDeMovimento); //chama minha outra função
+});
 
-function mostrarOpcoesDeMovimento(event) {
-  if (event.target === torre) {
-    const opcoesDeMovimento = document.createElement('div');
-    opcoesDeMovimento.className = 'opcoes-de-movimento';
+function mostrarOpcoesDeMovimento(event) { 
+  if (event.target.className === 'torre') { //verifica se a movimentação é da torre
+    const linha = event.target.parentElement.parentElement; //a 'linha' vai acessar o o avô do target, para chegar no elemento da linha
+    Array.from(linha.children).forEach(element => { //nos filhos da linha cria o loop da variavel element
+      if (element.children.length > 0) { //condicao de seguranca para que se há alguma obstrução, não marca como uma opção de movimento
+        return;
+      }
+      element.className = 'opcoes-de-movimento'; //classname adicionando a estilização na coluna que pode ser movimentada
+    });
+    const position = 
+    Array.from(linha.children).indexOf(event.target.parentElement); //identificando a coluna exata onde a peça está posicionada para marcar a mesma coluna na vertical
 
-    // código para adicionar opções de movimento a opcoesDeMovimento
+    const linhas = 
+    document.querySelectorAll('tr');    //crio uma variável selecionando todas as linhas (tr)
+    Array.from(linhas).forEach(linha => { //faz um loop para acessar linha por linha
+      const colunas = linha.children; //pego todas as colunas daquela linha
+      Array.from(colunas).forEach((coluna, index) => { //pra que eu entre na coluna e saiba qual a posição dela, e comparo com a variǘel de cima para saber se é a mesma ou não
+        if (index != position || coluna.className == 'position'){ //se a posição da coulna for diferente da peça ou se já tiver uma peça posicionada, nao estilizo
+          return;
+        }
+        coluna.className = 'opcoes-de-movimento'; //adicionando uma estilização pra coluna
+      });
+      
+    })
 
-    document.body.appendChild(opcoesDeMovimento);
-
-    const torreRect = torre.getBoundingClientRect();
-    const tabuleiroRect = tabuleiro.getBoundingClientRect();
-    const top = torreRect.top - tabuleiroRect.top + tabuleiro.scrollTop;
-    const left = torreRect.left - tabuleiroRect.left + tabuleiro.scrollLeft;
-
-    opcoesDeMovimento.style.top = `${top}px`;
-    opcoesDeMovimento.style.left = `${left}px`;
+   
   }
 }
 
-function esconderOpcoesDeMovimento() {
-  const opcoesDeMovimento = document.querySelector('.opcoes-de-movimento');
-  if (opcoesDeMovimento) {
-    opcoesDeMovimento.remove();
-  }
+function esconderOpcoesDeMovimento() { //quando tira o mouse ele remove a classe com a estilização
+  const opcoesDeMovimento = document.querySelectorAll('.opcoes-de-movimento');
+  Array.from(opcoesDeMovimento).forEach(element => {
+    element.className = '';
+  });
+  
 }
